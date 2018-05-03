ae91f35a8ad6b84de16ab21c8352d05d1cf6485b
zywoon93 <38690332+zywoon93@users.noreply.github.com>
2018-05-04 01:28:04 +0900

    Add files via upload

--- block.rb
+++ block.rb
@@ -1,1 +1,83 @@
-aa
+ require 'digest'
+ require 'securerandom'
+
+ class BlockHeader
+
+ 	def initialize
+ 		@time
+ 		@prevBlockHash
+ 		@nonce
+ 		@height
+ 	end
+
+ 	def prevBlockInfo
+ 	end
+
+ end
+
+ class Blockchain
+
+ 	def initialize
+ 		@chain = []
+ 		@transaction = []
+ 		@wallet = {}
+ 	end
+
+ 	def make_a_wallet
+ 	    address = SecureRandom.uuid.gsub("-", "")
+ 	    @wallet[address] = 100
+ 	    @wallet
+ 	end
+
+ 	def wallet_list
+ 		@wallet
+ 	end
+
+ 	def make_a_trans(sender, receiver, amount)
+ 		if 		@wallet[sender].nil?
+ 				"보내는 주소가 잘못되었습니다."
+ 		elsif 	@wallet[reciever].nil?
+ 				"받는 주소가 잘못되었습니다."
+ 		elsif   @wallet[sender].to_f < amount.to_f
+ 			    "돈이 부족합니다." 
+ 		else
+ 				@wallet[sender]   = @wallet[sender].to_f - amount.to_f
+ 				@wallet[receiver] = @wallet[receiver].to_f + amount.to_f
+
+ 		trans = {
+ 				"sender" => sender,
+ 				"recv"   => receiver
+ 				"amount" => amount
+ 				}
+ 		@transaction << trans
+ 				"다음 블록에 쓰여집니다." + (@chain.length + 1).to_s
+ 		end
+    end
+
+    def mining
+
+    	begin
+        		nonce  = rand(1000000)
+				hashed = Digest::SHA256.hexdigest(nonce.to_s)
+		end while hashed[0..3] != "0000"
+		block = {
+				"nHeight" 			=> @chain.size,
+				"nTime"	  			=> Time.now.to_i,
+				"nNonce"  			=> nonce,
+				"previous_address" 	=> Digest::SHA256.hexdigest(last_block.to_s),
+				"Transaction"		=> @transaction 
+				}		         		
+		@transaction =[]
+		@chain << block
+		end
+	end
+	
+	def last_block
+		@chain[-1]
+	end
+
+	def all_chains
+		@chain
+	end
+end
+
--- server.rb
+++ server.rb
@@ -1,1 +1,42 @@
-bb
+# encoding: UFT-8
+
+require 'sinatra'
+require './block'
+
+blockchain = BlockChain.new
+
+get '/' do 
+	message = "<center>"
+
+	blockchain.all_chains.each do |eachBlock|
+		message << "BlockHeight"	   : " + eachBlock['nHeight'].to_s + "<br>"
+		message << "Time"			   : " + eachBlock['nTime'].to_s + "<br>"
+		message << "Nonce"			   : " + eachBlock['nNonce'].to_s + "<br>"
+		message << "Previous_BlockHash : " + eachBlock['previous_address'].to_s + "<br>"
+		message << "Cur_BlockHash	   : " + Digest::SHA256.hexdigest(eachBlock.to_s) + "<br>"
+		message << "Cur_Transation	   : " + eachBlock['Transaction'].to_s + "<br>"
+		message << "<hr>"
+	end
+
+	message + "</center>"
+	message
+end
+
+get '/mine' do
+	blockchain.mining.to_s
+end
+
+get '/trans' do
+	blockchain.make_a_trans(params["sender"], params["recv"], params["amount"])
+end
+
+get '/new_wallet' do
+	blockchain.make_a_wallet.to_s
+end
+
+get 'wallet_list' do
+	blockchain.show_all_wallet.to_s
+end
+
+
+
